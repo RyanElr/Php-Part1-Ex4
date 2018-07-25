826abd028c52c3984d73ef1fc026f7daa14dd1e9
Ryan <ryan.elr@outlook.com>
2018-07-25 14:27:56 +0200

    ex4

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice4 Part1</title>
+  </head>
+  <body>
+    <?php
+    $text = 'Yo';
+    $number = 11;
+    $decimal = 11.55;
+    $bool = true;
+    echo $text.'<br/>';
+    echo $number.'<br/>';
+    echo $decimal.'<br/>';
+    echo $bool.'<br/>';
+    ?>
+  </body>
+</html>
